1661efea509fa8478e7aee911ce93dcc335c0587
Taras Oliynik <112812514+Tarasevich25@users.noreply.github.com>
2023-05-26 20:03:49 +0300

    updated

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,9 @@
-export async function fetchCountries(name) {
-    const response = await fetch(
+export function fetchCountries(name) {
+    const response =  fetch(
       `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
     );
     if (!response.ok) {
       throw new Error(response.status);
     }
-    return await response.json();
+    return  response.json();
   }
